39432842a3a251ae3e86b0d60f412de74fb52113
Jacques Ikot <jacquesikot@gmail.com>
2020-12-21 10:40:31 -0800

    v2

--- spider.js
+++ spider.js
@@ -3,6 +3,11 @@ import path from 'path';
 import superagent from 'superagent';
 import mkdirp from 'mkdirp';
 import { urlToFilename } from './utils.js';
+import fs from 'fs';
+import path from 'path';
+import superagent from 'superagent';
+import mkdirp from 'mkdirp';
+import { urlToFilename, getPageLinks } from './utils.js';
 
 function saveFile(filename, contents, cb) {
   mkdirp(path.dirname(filename), (err) => {
@@ -29,18 +34,54 @@ function download(url, filename, cb) {
   });
 }
 
-export function spider(url, cb) {
-  const filename = urlToFilename(url);
-  fs.access(filename, (err) => {
-    if (!err || err.code !== 'ENOENT') {
-      // [1] Early return principle
-      return cb(null, filename, false);
+function spiderLinks(currentUrl, body, nesting, cb) {
+  if (nesting === 0) {
+    // Zalgo
+    return process.nextTick(cb);
+  }
+
+  const links = getPageLinks(currentUrl, body); // [1]
+  if (links.length === 0) {
+    return process.nextTick(cb);
+  }
+
+  function iterate(index) {
+    // [2]
+    if (index === links.length) {
+      return cb();
     }
-    download(url, filename, (err) => {
+
+    spider(links[index], nesting - 1, function (err) {
+      // [3]
       if (err) {
         return cb(err);
       }
-      cb(null, filename, true);
+      iterate(index + 1);
     });
+  }
+
+  iterate(0); // [4]
+}
+
+export function spider(url, nesting, cb) {
+  const filename = urlToFilename(url);
+  fs.readFile(filename, 'utf8', (err, fileContent) => {
+    if (err) {
+      if (err.code !== 'ENOENT') {
+        return cb(err);
+      }
+
+      // The file doesn't exist, so let’s download it
+      return download(url, filename, (err, requestContent) => {
+        if (err) {
+          return cb(err);
+        }
+
+        spiderLinks(url, requestContent, nesting, cb);
+      });
+    }
+
+    // The file already exists, let’s process the links
+    spiderLinks(url, fileContent, nesting, cb);
   });
 }
