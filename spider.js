eea076b829c5cc6a766df6245345d1f6f272bd10
Jacques Ikot <jacquesikot@gmail.com>
2020-12-21 10:16:32 -0800

    fix with functions

--- spider.js
+++ spider.js
@@ -4,36 +4,43 @@ import superagent from 'superagent';
 import mkdirp from 'mkdirp';
 import { urlToFilename } from './utils.js';
 
+function saveFile(filename, contents, cb) {
+  mkdirp(path.dirname(filename), (err) => {
+    if (err) {
+      return cb(err);
+    }
+    fs.writeFile(filename, contents, cb);
+  });
+}
+
+function download(url, filename, cb) {
+  console.log(`Downloading ${url}`);
+  superagent.get(url).end((err, res) => {
+    if (err) {
+      return cb(err);
+    }
+    saveFile(filename, res.text, (err) => {
+      if (err) {
+        return cb(err);
+      }
+      console.log(`Downloaded and saved: ${url}`);
+      cb(null, res.text);
+    });
+  });
+}
+
 export function spider(url, cb) {
   const filename = urlToFilename(url);
   fs.access(filename, (err) => {
-    // [1]
-    if (err && err.code === 'ENOENT') {
-      console.log(`Downloading ${url} into ${filename}`);
-      superagent.get(url).end((err, res) => {
-        // [2]
-        if (err) {
-          cb(err);
-        } else {
-          mkdirp(path.dirname(filename), (err) => {
-            // [3]
-            if (err) {
-              cb(err);
-            } else {
-              fs.writeFile(filename, res.text, (err) => {
-                // [4]
-                if (err) {
-                  cb(err);
-                } else {
-                  cb(null, filename, true);
-                }
-              });
-            }
-          });
-        }
-      });
-    } else {
-      cb(null, filename, false);
+    if (!err || err.code !== 'ENOENT') {
+      // [1] Early return principle
+      return cb(null, filename, false);
     }
+    download(url, filename, (err) => {
+      if (err) {
+        return cb(err);
+      }
+      cb(null, filename, true);
+    });
   });
 }
